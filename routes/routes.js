682cfd7ec91cb0bc2c02e5f0761ddfe8cd80c2f6
gshaver <geneshaver@gmail.com>
2020-07-30 21:46:45 -0500

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -7,7 +7,7 @@ module.exports = function (app) {
     app.get("/stats", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/stats.html"));
     });    
-    app.get("/", (req, res) => {
+    app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 };
